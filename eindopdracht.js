62e14ee3aacdc42c33465e85ad4f88f026bda1c7
Robin <robindev2019@outlook.com>
2025-03-03 08:28:02 +0100

    Abstract common logic

--- eindopdracht.js
+++ eindopdracht.js
@@ -1,62 +1,47 @@
+const count = (condition) => {
+  return (database) => {
+    return query(condition)(database);
+  };
+};
+
+const query = (condition) => {
+  return (database) => {
+    let acc = [];
+    for (const p of database) {
+      if (condition(p)) acc.push(p.naam);
+    }
+    return acc;
+  };
+};
+
 const vragen = [
   {
     vraag: "Hoeveel personen zijn er ouder dan 35 jaar?",
-    antwoord: (database) => {
-      let n = 0;
-      for (const p of database) if (p.leeftijd > 35) n++;
-      return n;
-    },
+    antwoord: count((p) => p.leeftijd > 35),
   },
   {
     vraag: "Wie heeft er meer dan 2 kenissen?",
-    antwoord: (database) => {
-      let n = [];
-      for (const p of database) if (p.kennissen.length > 2) n.push(p);
-      return n;
-    },
+    antwoord: query((p) => p.kennissen.length > 2),
   },
   {
     vraag: "Wie is er op vakantie?",
-    antwoord: (database) => {
-      let n = [];
-      for (const p of database) if (p.opVakantie) n.push(p);
-      return n;
-    },
+    antwoord: query((p) => p.opVakantie),
   },
   {
     vraag: "Wie is er niet op vakantie?",
-    antwoord: (database) => {
-      let n = [];
-      for (const p of database) if (!p.opVakantie) n.push(p);
-      return n;
-    },
+    antwoord: query((p) => !p.opVakantie),
   },
   {
     vraag: "Wie is jonger dan 30 of ouder dan 50?",
-    antwoord: (database) => {
-      let n = [];
-      for (const p of database)
-        if (p.leeftijd < 30 || p.leeftijd > 50) n.push(p);
-      return n;
-    },
+    antwoord: query((p) => p.leeftijd < 30 || p.leeftijd > 50),
   },
   {
     vraag: "Wie is er op vakantie in Berlijn?",
-    antwoord: (database) => {
-      let n = [];
-      for (const p of database)
-        if (p.locatie.stad == "Berlijn" && p.opVakantie) n.push(p);
-      return n;
-    },
+    antwoord: query((p) => p.locatie.stad == "Berlijn" && p.opVakantie),
   },
   {
     vraag: "Wie is er niet op vakantie in Amsterdam?",
-    antwoord: (database) => {
-      let n = [];
-      for (const p of database)
-        if (p.locatie.stad == "Amsterdam" && p.opVakantie) n.push(p);
-      return n;
-    },
+    antwoord: query((p) => p.locatie.stad == "Amsterdam" && p.opVakantie),
   },
   {
     vraag: "Wie heeft er meer dan 2 kenissen?",
@@ -77,7 +62,9 @@ const vragen = [
 
       for (const p of database) {
         for (const e of p.emails) {
-          if (e.endsWith("@hotmail.com")) n.push(p);
+          if (e.endsWith("@hotmail.com") && !n.includes(p.naam)) {
+            n.push(p.naam);
+          }
         }
       }
 
