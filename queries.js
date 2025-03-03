bd229edb3ceb0be650b5295ff2bf8b4f5e5b72bc
Robin <robindev2019@outlook.com>
2025-03-03 13:40:31 +0100

    Make the DuckDB work!!

--- queries.js
+++ queries.js
@@ -1,43 +1,39 @@
 const queries = [
   {
     title: "Hoeveel personen zijn er ouder dan 35 jaar?",
-    query: "SELECT 'hello world' as friendly_greeting",
+    query: "SELECT COUNT(*)::int as amount FROM people WHERE leeftijd > 35",
   },
   {
-    title: "Wie heeft er meer dan 2 kenissen?",
-    query: "SELECT 'hello world' as friendly_greeting",
+    title: "Wie heeft er meer dan 2 kennissen?",
+    query: "SELECT naam, len(kennissen) as aantal FROM people WHERE len(kennissen) > 2",
   },
   {
     title: "Wie is er op vakantie?",
-    query: "SELECT 'hello world' as friendly_greeting",
+    query: "SELECT naam FROM people WHERE opVakantie = true",
   },
   {
     title: "Wie is er niet op vakantie?",
-    query: "SELECT 'hello world' as friendly_greeting",
+    query: "SELECT naam FROM people WHERE opVakantie = false",
   },
   {
     title: "Wie is jonger dan 30 of ouder dan 50?",
-    query: "SELECT 'hello world' as friendly_greeting",
+    query: "SELECT naam, leeftijd FROM people WHERE leeftijd < 30 OR leeftijd > 50",
   },
   {
     title: "Wie is er op vakantie in Berlijn?",
-    query: "SELECT 'hello world' as friendly_greeting",
+    query: "SELECT naam FROM people WHERE opVakantie = true AND locatie.stad = 'Berlijn'",
   },
   {
     title: "Wie is er niet op vakantie in Amsterdam?",
-    query: "SELECT 'hello world' as friendly_greeting",
+    query: "SELECT naam, opVakantie, locatie.stad FROM people WHERE opVakantie = false AND locatie.stad = 'Amsterdam'",
   },
   {
-    title: "Wie heeft er meer dan 2 kenissen?",
-    query: "SELECT 'hello world' as friendly_greeting",
+    title: "Wie is er niet op vakantie in Amsterdam?",
+    query: "SELECT naam, opVakantie, locatie.stad FROM people WHERE opVakantie = true AND locatie.stad != 'Amsterdam'",
   },
   {
-    title: "Wie heeft er meer dan 2 kenissen?",
-    query: "SELECT 'hello world' as friendly_greeting",
-  },
-  {
-    title: "Wie heeft er tenminste 1 '@hotmail.com' mail adres?",
-    query: "SELECT 'hello world' as friendly_greeting",
+    title: "Wie heeft er tenminste één '@hotmail.com' mail adres?",
+    query: "SELECT naam, emails FROM people WHERE EXISTS (SELECT true FROM UNNEST(emails) AS t(e) WHERE e LIKE '%hotmail.com')",
   },
 ];
 
